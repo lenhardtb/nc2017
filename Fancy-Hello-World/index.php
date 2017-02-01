0c32a8ffb3d30d1c9e37b2d87aebfacb3930a3f7
lenhardtb <b-lenhardt@onu.edu>
2017-01-31 20:07:15 -0500

    Add files via upload

---
+++ Fancy-Hello-World/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <link rel="stylesheet" href="FancyHelloWorld.css">
+        <meta charset="UTF-8">
+        <title>Hello, World from around the world!</title>
+    
+        
+    </head>
+    <body>
+        <div id="US" class="language"><img src="imgfolder/us flag.png">Hello, World!</div>
+        <div id="Chinese" class="language"><img src="imgfolder/chinese flag.png">喂 人类社会</div>
+        <div id="French" class="language"><img src="imgfolder/french flag.png">Bonjour à la monde!</div>
+        <div id="German" class="language"><img src="imgfolder/german flag.png">Hallo, der Welt!</div>
+        <div id="Russian" class="language"><img src="imgfolder/russian flag.png">привет, человечество</div>
+        <div id="Finnish" class="language"><img src="imgfolder/finnish flag.png">Hei, maailma!</div>
+        <div id="Swedish" class="language"><img src="imgfolder/swedish flag.png">Hallå Värld!</div>
+        <div id="Spanish" class="language"><img src="imgfolder/spanish flag.png">¡Hola, el mundo!</div>
+        <div id="Japanese" class="language"><img src="imgfolder/japanese flag.png">こんにちは 世界</div>
+        <div id="Swahili" class="language"><img src="imgfolder/kenyan flag.png">hodi dunia</div>
+        <div id="Arabic" class="language"><img src="imgfolder/saudi flag.png">البشريأهلاًة</div>
+        <div id="Maori" class="language"><img src="imgfolder/new zealander flag.png">Tena ao!</div>
+        
+        <img id="Earth" src="imgFolder/earth.png">
+    </body>
+</html>
