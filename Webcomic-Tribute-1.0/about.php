70db1b2e5c39cd2a5a64d17cdbd10787c6e774d0
lenhardtb <b-lenhardt@onu.edu>
2017-02-05 19:28:14 -0500

    Add files via upload

---
+++ Webcomic-Tribute-1.0/about.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <link rel = "stylesheet" href="Main.css">
+        <meta charset="UTF-8">
+        <title>Webcomic Collective V1 - About</title>
+    </head>
+    <body>
+        <p id="menu">
+            <a href="index.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="honMent.php">Honorable Mentions</a>
+        </p>
+        <div id ="mainDiv">
+            <ul>
+                <li><a href="webcomics.php">Don't know what a webcomic is?</a></li>
+                <li>This was made for Net-Centric Computing class by Bo Lenhardt, finished on February 1st, 2017.</li>
+                <li>Bo has been a fan of webcomics for a few years, so this seemed like a good idea to create some content for.</li>
+                <li>They are aware that these are 90% fantasy and will never apologize for it.</li>
+                <li>There will probably be many more projects about webcomics because Bo is not creative.</li>
+            </ul>
+        </div>
+    </body>
+</html>
---
+++ Webcomic-Tribute-1.0/webcomics.php
@@ -0,0 +1,37 @@
+<html>
+    <head>
+        <link rel = "stylesheet" href="Main.css">
+        <meta charset="UTF-8">
+        <title>Webcomic Collective V1 - What's A Webcomic?</title>
+    </head>
+    <body>
+        <p id="menu">
+            <a href="index.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="honMent.php">Honorable Mentions</a>
+        </p>
+        <p id="title">
+            <strong><u><i>So what's a webcomic???</i></u></strong>
+            </p>
+        <div id="mainDiv">
+            
+            <p id="webcomicDescription">
+                Webcomics are an interesting development in the interaction between the popular comic format and the internet.
+                Many artists have started publishing comics of their own, one page or one strip at a time. 
+                <br><br>Some profit off of it. Some do it for fun.
+                <br><br>Most will post one or two pages of a comic per week, but there are exceptions - almost all have strict update schedules, though. 
+                Some update monthly or bimonthly. A few update with single strips every day!
+                <br><br>The webcomic community is surprisingly compassionate. 
+                The artists behind comics are people, after all, and sometimes they need breaks -
+                not to mention that most are doing it for free or in their spare time anyway!
+                <br><br>When a comic goes <i>on hiatus</i>, responses are usually overwhelmingly forgiving because of this widely accepted mentality.
+                Few will only read one or two comics, however, so even so you can usually just keep receiving updates from other comics.
+                It's not long before you get hooked and suddenly you have dozens of your favorites' schedules memorized and a hundred bookmarks besides.
+                <br><br>It's important to pace yourself and not look for new ones when you're too busy. Happy hunting!
+                
+                <br><br><i>*psst! services like <a href="http://www.tapastic.com">tapastic.com</a> are helpful for keeping track of comics' updates. But be careful to remember that there are people behind these - and none of them owes you anything! Don't be a jerk.*</i>
+                
+            </p>
+        </div>
+    </body>
+</html>
